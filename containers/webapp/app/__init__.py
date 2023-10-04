60b847276d03883e4f9ddc4d46e647b5eb97a470
Diego Pereiro Rodriguez <58935004+dpereiro91@users.noreply.github.com>
2023-10-04 23:07:06 +0100

    Update __init__.py

--- containers/webapp/app/__init__.py
+++ containers/webapp/app/__init__.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 from werkzeug.debug import DebuggedApplication
 app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
 
-app.debug = True
+app.debug = False
 from app import views
